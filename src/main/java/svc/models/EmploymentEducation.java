cebe269f1afa201bdac3992920d9952764d6407a
Williams <KDW0609@daugherty.com>
2016-10-21 23:18:50 -0500

    Added EmploymentEducation

---
+++ src/main/java/svc/models/EmploymentEducation.java
@@ -0,0 +1,21 @@
+package svc.models;
+
+import java.util.Date;
+
+public class EmploymentEducation {
+	public int id;
+	public int project_entry_id;
+	public int personal_id;
+	public Date information_date;
+	public Integer last_grade_completed;
+	public Integer school_status;
+	public Integer employed_status;
+	public Integer employment_type;
+	public Integer not_employed_reason;
+	public int data_collection_stage;
+	public Date date_created;
+	public Date date_updated;
+	public int user_id;
+	public Date date_deleted;
+	public int export_id;
+}
