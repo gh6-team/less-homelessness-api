d6c7bf9478af7b6eb4c38bbf9d16fabd1756fb87
Williams <KDW0609@daugherty.com>
2016-10-21 23:04:51 -0500

    Add VeteranStatus

--- src/main/java/svc/models/VeteranStatus.java
+++ src/main/java/svc/models/VeteranStatus.java
@@ -1,5 +1,25 @@
 package svc.models;
 
 public enum VeteranStatus {
+	NO,
+	YES,
+	DOES_NOT_KNOW,
+	REFUSED,
+	NO_DATA,
+	OTHER;
+	
+	static VeteranStatus fromColumn(int status)
+	{	
+		switch(status)
+		{
+		case 0: return NO;
+		case 1: return YES;
+		case 8: return DOES_NOT_KNOW;
+		case 9: return REFUSED;
+		case 99:
+		default:
+			return NO_DATA;
+		}
+	}
 
 }
