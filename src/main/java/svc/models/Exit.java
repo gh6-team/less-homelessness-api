eb132edfc7fa68f362b729f8cf4bb2ae9f9d11fb
Williams <KDW0609@daugherty.com>
2016-10-21 23:36:40 -0500

    Exit class

---
+++ src/main/java/svc/models/Exit.java
@@ -0,0 +1,21 @@
+package svc.models;
+
+import java.util.Date;
+
+public class Exit {
+	public int id;
+	public int project_entry_id;
+	public int personal_id;
+	public Date exit_date;
+	public Integer destination;
+	public Integer housing_assessment;
+	public Integer subsidy_information;
+	public Integer project_completion_status;
+	public Integer early_exit_reason;
+	public Date date_created;
+	public Date date_updated;
+	public int user_id;
+	public Date date_deleted;
+	public int export_id;
+	
+}
