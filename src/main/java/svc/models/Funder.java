a9a384983a22fcda270a3dbff4f77f30f970d5bf
Williams <KDW0609@daugherty.com>
2016-10-21 23:38:51 -0500

    Funder class

---
+++ src/main/java/svc/models/Funder.java
@@ -0,0 +1,18 @@
+package svc.models;
+
+import java.util.Date;
+
+public class Funder {
+	public int id;
+	public int project_id;
+	public int funder;
+	public Integer grant_id;
+	public Date start_date;
+	public Date end_date;
+	public Date date_created;
+	public Date date_updated;
+	public Date date_deleted;
+	public int user_id;
+	public int export_id;
+	
+}
