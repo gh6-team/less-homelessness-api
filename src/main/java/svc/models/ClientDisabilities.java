f9655b9a8e5d0880adf09020237befdf2f081d0b
Williams <KDW0609@daugherty.com>
2016-10-21 23:12:44 -0500

    finished ClientDisabilities

--- src/main/java/svc/models/ClientDisabilities.java
+++ src/main/java/svc/models/ClientDisabilities.java
@@ -1,5 +1,30 @@
 package svc.models;
 
-public class ClientDisabilities {
+import java.util.Date;
 
+public class ClientDisabilities {
+	public String id;
+	public int project_entry_id;
+	public int personal_id;
+	public Date information_date;
+	public int disability_type;
+	public int disability_response;
+	public Integer indefinite_and_impairs;
+	public Integer documentation_on_file;
+	public Integer receiving_services;
+	public Integer path_how_confirmed;
+	public Integer path_smi_information;
+	public Boolean is_tcell_count_available;
+	public Integer tcell_count;
+	public Integer tcell_source;
+	public Boolean is_viral_load_available;
+	public Integer viral_load;
+	public Integer viral_load_source;
+	public int data_collection_stage;
+	public Date date_created;
+	public Date date_updated;
+	public int user_id;
+	public Date date_deleted;
+	public int export_id;
+	
 }
