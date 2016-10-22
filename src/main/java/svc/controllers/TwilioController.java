782a01b244ba8dbea8a71898a6a6269a6eaa309d
Kyle Williams <d.kyle.williams@gmail.com>
2016-10-22 11:19:36 -0500

    Initial twilio controller

---
+++ src/main/java/svc/controllers/TwilioController.java
@@ -0,0 +1,28 @@
+package svc.controllers;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.twilio.twiml.Body;
+import com.twilio.twiml.Message;
+import com.twilio.twiml.MessagingResponse;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/twilio")
+public class TwilioController {
+
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.POST)
+	String Inbound(@RequestParam String from, @RequestBody String body)
+	{
+		Message twilioMessage = new Message.Builder().body(new Body("Message from: " + from)).build();
+		MessagingResponse twilioResponse = new MessagingResponse.Builder().message(twilioMessage).build();
+		return twilioResponse.toString();
+	}
+}
---
+++ src/main/java/svc/managers/TwilioManager.java
@@ -0,0 +1,5 @@
+package svc.managers;
+
+public class TwilioManager {
+
+}
